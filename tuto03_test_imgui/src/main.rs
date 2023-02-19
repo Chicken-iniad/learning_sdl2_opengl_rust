ed821d966a510b9e00a1c5765d51adefb88f14b4
Chicken-iniad <s1f102200075@iniad.org>
2023-02-19 09:13:48 +0900

    imgui

--- tuto03_test_imgui/src/main.rs
+++ tuto03_test_imgui/src/main.rs
@@ -2,11 +2,11 @@ use std::mem;
 use std::os::raw::c_void;
 use std::time::Duration;
 
-
 use c_str_macro::c_str;
 use cgmath::perspective;
 use cgmath::prelude::SquareMatrix;
 use gl::types::{GLfloat, GLsizei, GLsizeiptr};
+use imgui::im_str;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
@@ -32,7 +32,7 @@ const BUF_LEN: usize = FLOAT_NUM * VERTEX_NUM;
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
+    
     {
         let gl_attr = video_subsystem.gl_attr();
         gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
@@ -54,7 +54,6 @@ fn main() {
     let shader = Shader::new("rsc/shader/shader.vs", "rsc/shader/shader.fs");
 
     // set buffer
-    
     #[rustfmt::skip]
     let buffer_array: [f32; BUF_LEN] = [
         -1.0, -1.0, 0.0,
@@ -72,11 +71,24 @@ fn main() {
         VERTEX_NUM as i32,
     );
 
-    
+    // init imgui
+    let mut imgui_context = imgui::Context::create();
+    imgui_context.set_ini_filename(None);
+
+    // init imgui sdl2
+    let mut imgui_sdl2_context = imgui_sdl2::ImguiSdl2::new(&mut imgui_context, &window);
+    let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui_context, |s| {
+        video_subsystem.gl_get_proc_address(s) as _
+    });
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
+            imgui_sdl2_context.handle_event(&mut imgui_context, &event);
+            if imgui_sdl2_context.ignore_event(&event) {
+                continue;
+            }
+
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
@@ -107,7 +119,7 @@ fn main() {
                     y: 0.0,
                     z: 0.0,
                 },
-                Vector3 { 
+                Vector3 {
                     x: 0.0,
                     y: 1.0,
                     z: 0.0,
@@ -127,6 +139,49 @@ fn main() {
             shader.set_mat4(c_str!("uProjection"), &projection_matrix);
 
             vertex.draw();
+
+            imgui_sdl2_context.prepare_frame(
+                imgui_context.io_mut(),
+                &window,
+                &event_pump.mouse_state(),
+            );
+
+            let ui = imgui_context.frame();
+            imgui::Window::new(im_str!("Information"))
+                .size([300.0, 200.0], imgui::Condition::FirstUseEver)
+                .build(&ui, || {
+                    ui.text(im_str!("OpenGL Test App ver 1.0"));
+                    ui.separator();
+                    ui.text(im_str!("FPS: {:.1}", ui.io().framerate));
+                    let display_size = ui.io().display_size;
+                    ui.text(format!(
+                        "Display Size: ({:.1}, {:.1})",
+                        display_size[0], display_size[1]
+                    ));
+                    let mouse_pos = ui.io().mouse_pos;
+                    ui.text(format!(
+                        "Mouse Position: ({:.1}, {:.1})",
+                        mouse_pos[0], mouse_pos[1]
+                    ));
+                    imgui::ProgressBar::new(mouse_pos[0] / 640f32)
+                        .size([200.0, 20.0])
+                        .overlay_text(im_str!("mouse-X"))
+                        .build(&ui);
+                    imgui::ProgressBar::new(mouse_pos[1] / 480f32)
+                        .size([200.0, 20.0])
+                        .overlay_text(im_str!("mouse-Y"))
+                        .build(&ui);
+                    let arr = [0.2, 0.4, 0.8, 0.1, 1.0, 0.7];
+                    ui.plot_lines(im_str!("lines"), &arr)
+                        .graph_size([200.0, 40.0])
+                        .build();
+                    ui.plot_histogram(im_str!("histogram"), &arr)
+                        .graph_size([200.0, 40.0])
+                        .build();
+                });
+
+            imgui_sdl2_context.prepare_render(&ui, &window);
+            renderer.render(ui);
 
             window.gl_swap_window();
         }
